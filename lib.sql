67320d23105e1043995148a4fa1c824320c367bd
ccccmd <43992619+ccccmd@users.noreply.github.com>
2018-12-20 14:46:38 +0800

    Add files via upload

--- lib.sql
+++ lib.sql
@@ -27,7 +27,11 @@ CREATE TABLE `app_student` (
   `stu_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `app_data` date NOT NULL,
-  PRIMARY KEY (`app_student_id`)
+  PRIMARY KEY (`app_student_id`),
+  KEY `FK_app_stu_id` (`stu_id`),
+  KEY `FK_app_book_id` (`book_id`),
+  CONSTRAINT `FK_app_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_app_stu_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,7 +56,11 @@ CREATE TABLE `app_teacher` (
   `tea_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `app_date` date NOT NULL,
-  PRIMARY KEY (`app_teacher_id`)
+  PRIMARY KEY (`app_teacher_id`),
+  KEY `FK_app_tea_id` (`tea_id`),
+  KEY `FK_app_tbook_id` (`book_id`),
+  CONSTRAINT `FK_app_tbook_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_app_tea_id` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,15 +82,19 @@ DROP TABLE IF EXISTS `book`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `book` (
   `book_id` int(11) NOT NULL AUTO_INCREMENT,
-  `book_name` varchar(20) NOT NULL,
-  `book_intro` varchar(20) NOT NULL,
-  `book_addr` varchar(20) NOT NULL,
-  `book_ok` varchar(20) NOT NULL,
+  `book_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `book_intro` text,
+  `book_addr` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `book_ok` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `book_health` int(11) NOT NULL,
-  `sort_id` int(11) NOT NULL,
   `book_record` date DEFAULT NULL,
   `book_borrow_times` int(11) NOT NULL,
-  `book_price` int(11) NOT NULL,
+  `book_price` double DEFAULT NULL,
+  `book_sort` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `manager_email` varchar(20) NOT NULL,
+  `manager_cardid` varchar(20) NOT NULL,
+  `manager_code` varchar(20) NOT NULL,
+  `book_aut` varchar(20) NOT NULL,
   PRIMARY KEY (`book_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -94,30 +106,6 @@ CREATE TABLE `book` (
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `book_sort`
---
-
-DROP TABLE IF EXISTS `book_sort`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `book_sort` (
-  `sort_id` int(11) NOT NULL AUTO_INCREMENT,
-  `sort_name` varchar(20) NOT NULL,
-  `sort_sit` varchar(20) NOT NULL,
-  PRIMARY KEY (`sort_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `book_sort`
---
-
-LOCK TABLES `book_sort` WRITE;
-/*!40000 ALTER TABLE `book_sort` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book_sort` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -133,7 +121,11 @@ CREATE TABLE `borrow_student` (
   `book_id` int(11) NOT NULL,
   `borrow_date` date NOT NULL,
   `exp_return_date` date NOT NULL,
-  PRIMARY KEY (`borrow_student_id`)
+  PRIMARY KEY (`borrow_student_id`),
+  KEY `FK_bor_stu_id` (`stu_id`),
+  KEY `FK_bor_book_id` (`book_id`),
+  CONSTRAINT `FK_bor_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_bor_stu_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -159,7 +151,11 @@ CREATE TABLE `borrow_teacher` (
   `book_id` int(11) NOT NULL,
   `borrow_date` date NOT NULL,
   `exp_return_date` date NOT NULL,
-  PRIMARY KEY (`borrow_teacher_id`)
+  PRIMARY KEY (`borrow_teacher_id`),
+  KEY `FK_bor_tea_id` (`tea_id`),
+  KEY `FK_bor_tbook_id` (`book_id`),
+  CONSTRAINT `FK_bor_tbook_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_bor_tea_id` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -184,8 +180,12 @@ CREATE TABLE `col_student` (
   `stu_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `col_date` date NOT NULL,
-  `col_instr` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`col_student_id`)
+  `col_instr` text,
+  PRIMARY KEY (`col_student_id`),
+  KEY `FK_col_stu_id` (`stu_id`),
+  KEY `FK_col_book_id` (`book_id`),
+  CONSTRAINT `FK_col_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_col_stu_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -210,8 +210,12 @@ CREATE TABLE `col_teacher` (
   `tea_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `col_date` date NOT NULL,
-  `col_instr` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`col_teacher_id`)
+  `col_instr` text,
+  PRIMARY KEY (`col_teacher_id`),
+  KEY `FK_col_tea_id` (`tea_id`),
+  KEY `FK_col_tbook_id` (`book_id`),
+  CONSTRAINT `FK_col_tbook_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_col_tea_id` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -263,7 +267,13 @@ CREATE TABLE `return_student` (
   `return_date` date NOT NULL,
   `manager_id` int(11) NOT NULL,
   `book_health` int(11) NOT NULL,
-  PRIMARY KEY (`return_student_id`)
+  PRIMARY KEY (`return_student_id`),
+  KEY `FK_ret_stu_id` (`stu_id`),
+  KEY `FK_ret_book_id` (`book_id`),
+  KEY `FK_ret_man_id` (`manager_id`),
+  CONSTRAINT `FK_ret_book_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_ret_man_id` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`manager_id`),
+  CONSTRAINT `FK_ret_stu_id` FOREIGN KEY (`stu_id`) REFERENCES `student` (`stu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -290,7 +300,13 @@ CREATE TABLE `return_teacher` (
   `return_date` date NOT NULL,
   `manager_id` int(11) NOT NULL,
   `book_health` int(11) NOT NULL,
-  PRIMARY KEY (`return_teacher_id`)
+  PRIMARY KEY (`return_teacher_id`),
+  KEY `FK_ret_tea_id` (`tea_id`),
+  KEY `FK_ret_tbook_id` (`book_id`),
+  KEY `FK_ret_tman_id` (`manager_id`),
+  CONSTRAINT `FK_ret_tbook_id` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`),
+  CONSTRAINT `FK_ret_tea_id` FOREIGN KEY (`tea_id`) REFERENCES `teacher` (`tea_id`),
+  CONSTRAINT `FK_ret_tman_id` FOREIGN KEY (`manager_id`) REFERENCES `manager` (`manager_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -317,6 +333,11 @@ CREATE TABLE `student` (
   `stu_age` int(11) NOT NULL,
   `stu_pro` varchar(20) NOT NULL,
   `stu_grade` int(11) NOT NULL,
+  `stu_username` varchar(20) NOT NULL,
+  `stu_passwd` varchar(20) NOT NULL,
+  `stu_email` varchar(20) NOT NULL,
+  `stu_cardid` varchar(20) NOT NULL,
+  `stu_code` varchar(20) NOT NULL,
   PRIMARY KEY (`stu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -343,6 +364,11 @@ CREATE TABLE `teacher` (
   `tea_sex` varchar(5) NOT NULL,
   `tea_age` int(11) NOT NULL,
   `tea_dep` varchar(20) NOT NULL,
+  `tea_username` varchar(20) NOT NULL,
+  `tea_passwd` varchar(20) NOT NULL,
+  `tea_email` varchar(20) NOT NULL,
+  `tea_cardid` varchar(20) NOT NULL,
+  `tea_code` varchar(20) NOT NULL,
   PRIMARY KEY (`tea_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -355,58 +381,6 @@ LOCK TABLES `teacher` WRITE;
 /*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
 /*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `ticket_student`
---
-
-DROP TABLE IF EXISTS `ticket_student`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `ticket_student` (
-  `ticket_student_id` int(11) NOT NULL AUTO_INCREMENT,
-  `stu_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `over_date` int(11) NOT NULL,
-  `ticket_fee` int(11) NOT NULL,
-  PRIMARY KEY (`ticket_student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_student`
---
-
-LOCK TABLES `ticket_student` WRITE;
-/*!40000 ALTER TABLE `ticket_student` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_student` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket_teacher`
---
-
-DROP TABLE IF EXISTS `ticket_teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `ticket_teacher` (
-  `ticket_teacher_id` int(11) NOT NULL AUTO_INCREMENT,
-  `tea_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `over_date` int(11) NOT NULL,
-  `ticket_fee` int(11) NOT NULL,
-  PRIMARY KEY (`ticket_teacher_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ticket_teacher`
---
-
-LOCK TABLES `ticket_teacher` WRITE;
-/*!40000 ALTER TABLE `ticket_teacher` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket_teacher` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -417,4 +391,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-20 12:44:49
+-- Dump completed on 2018-12-20 14:45:31
