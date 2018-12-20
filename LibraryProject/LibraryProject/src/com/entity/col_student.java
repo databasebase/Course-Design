5d40fa5f59af7cba8f4d55bd4f4ef45a20592203
Runzhe He <ssfldsh@gmail.com>
2018-12-20 18:43:08 +0800

    Add files via upload

---
+++ LibraryProject/LibraryProject/src/com/entity/col_student.java
@@ -0,0 +1,40 @@
+package com.entity;
+
+public class col_student {
+	private int col_student_id;
+	private int stu_id;
+	private int book_id;
+	private String col_date;
+	private String col_instr;
+	public int getCol_student_id() {
+		return col_student_id;
+	}
+	public void setCol_student_id(int col_student_id) {
+		this.col_student_id = col_student_id;
+	}
+	public int getStu_id() {
+		return stu_id;
+	}
+	public void setStu_id(int stu_id) {
+		this.stu_id = stu_id;
+	}
+	public int getBook_id() {
+		return book_id;
+	}
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+	public String getCol_date() {
+		return col_date;
+	}
+	public void setCol_date(String col_date) {
+		this.col_date = col_date;
+	}
+	public String getCol_instr() {
+		return col_instr;
+	}
+	public void setCol_instr(String col_instr) {
+		this.col_instr = col_instr;
+	}
+	
+}
