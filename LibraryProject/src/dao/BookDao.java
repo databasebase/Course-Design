d38dad0df7ae535c146334b2875ed8ef6fd6a810
ccccmd <43992619+ccccmd@users.noreply.github.com>
2018-12-22 08:39:38 +0800

    use the set and get to insert

--- LibraryProject/src/dao/BookDao.java
+++ LibraryProject/src/dao/BookDao.java
@@ -1,36 +1,23 @@
 package dao;
 
+import java.awt.print.Book;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.entity.AppStudent;
 import com.entity.book;
 
 public class BookDao  extends C3p0Connection{
 		//向书表中插入新书
-		public void Insert(int book_id,String book_name,String book_aut,String book_intro,String book_addr,String book_ok,int book_health,String book_record,int book_borrow_times,double book_price,String book_sort,int book_state)
+	public void Insert(book b)
 		{
 			try {
 				Connection cn = null;
 				Statement st = null;
-				book b = new book();
-				b.setBook_id(book_id);
-				b.setBook_name(book_name);
-				b.setBook_aut(book_aut);
-				b.setBook_intro(book_intro);
-				b.setBook_addr(book_addr);
-				b.setBook_ok(book_ok);
-				b.setBook_health(book_health);
-				b.setBook_record(book_record);
-				b.setBook_borrow_times(book_borrow_times);
-				b.setBook_price(book_price);
-				b.setBook_sort(book_sort);
-				b.setBook_state(book_state);
 				cn = getConnection();
 				st = cn.createStatement();
-				String sql = "insert into book (book_id,book_name,book_intro,book_addr,book_ok,book_health,book_record,book_borrow_times,book_price,book_sort,book_aut,book_state)values(book_id,book_name,book_intro,book_addr,book_ok,book_health,book_record,book_borrow_times,book_price,book_sort,book_aut,book_state)";
+				String sql = "insert into book(book_id,book_name,book_aut,book_intro,book_addr,book_ok,book_health,book_record,book_borrow_times,book_price,book_sort,book_state) values('"+b.getBook_id()+"','"+b.getBook_name()+"','"+b.getBook_aut()+"','"+b.getBook_intro()+"','"+b.getBook_addr()+"','"+b.getBook_ok()+"','"+b.getBook_health()+"','"+b.getBook_record()+"','"+b.getBook_borrow_times()+"','"+b.getBook_price()+"','"+b.getBook_sort()+"','"+b.getBook_state()+"')";
 				boolean rs = st.execute(sql);
 				System.out.println("SUCCESS");
 			}catch(SQLException e)
